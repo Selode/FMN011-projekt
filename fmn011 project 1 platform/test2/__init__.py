3bcc1547f9931030228a77394e72889e364c6cdb
Martin <martin.je.bergman@gmail.com>
2017-04-05 22:56:11 +0200

    final touches

--- fmn011 project 1 platform/test2/__init__.py
+++ fmn011 project 1 platform/test2/__init__.py
@@ -1,7 +1,12 @@
+import math
 import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
-import math
+import matplotlib.colors as colors
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
 
 
 
@@ -17,14 +22,13 @@ def task1(L, b, d):
 def task2(L, b, d, a):
     XT = (2.4537, -4.9075, 2.7424)
     print(XT)
-
     P = constructP(L, b, d)
     H = constructH(L, P)
     XP = XPequations(P, b, d)
     YP = YPequations(P, b, d)
 
-    data = a, b, d, H, L, P, XP, YP
-    print stf(XT, *data)
+
+    print (stf(XT, a, b, d, H, L, P, XP, YP))
     return
 
 
@@ -36,41 +40,32 @@ def task4(L, b, d, a):
     YP = YPequations(P, b, d)
     XT = (2.4537, -4.9075, 2.7424)
 
-    data = a, b, d, H, L, P, XT, XP, YP
-    print fsolve(stf, *data)
+    print (fsolve(stf, XT, (a, b, d, H, L, P, XP, YP)))
 
 
 def task5(L, b, d, a):
     startXT = (2.4537, -4.9075, 2.7424)
     # Legs at min length:
-    L = (8, 8, 8, 8, 8, 8)
-
-    XT = (fsolve(stf, startXT, (L,b,d,a)))
+    #L = (8, 8, 8, 8, 8, 8)
+    #Legs at max length:
+    #L = (15, 15, 15, 15, 15, 15)
+    # Maximally tilted platform:
+    #L = (15, 15, 8, 8, 8, 8)
+    # Maximally twisted platform:
+    L = (15, 15, 8, 15, 8, 8)
     P = constructP(L, b, d)
     H = constructH(L, P)
     XP = XPequations(P, b, d)
     YP = YPequations(P, b, d)
+    XT = (fsolve(stf, startXT, (a, b, d, H, L, P, XP, YP)))
     YT = calcYT(XP, XT, YP)
     ZT = calcZT(H, XP, XT)
-    #Legs at max length:
-
-    calculatedXT = (fsolve(stf, startXT, (L, b, d, a)))
-
-    # Legs at max length:
-
-    L = (15, 15, 15, 15, 15, 15)
-    calculatedXT = (fsolve(stf, startXT, (L, b, d, a)))
-
-    # Maximally tilted platform:
-    L = (15, 15, 8, 8, 8, 8)
-    calculatedXT = (fsolve(stf, startXT, (L, b, d, a)))
-
-    # Maximally twisted platform:
-    L = (8, 15, 8, 15, 8, 15)
-    
-
-    calculatedXT = (fsolve(stf, startXT, (L, b, d, a)))
-
+    XB = calcBaseX(b, d)
+    YB = calcBaseY(b, d)
+    ZB = np.zeros(6)
+    (L1, L2, L3) = calcLegs(XT, YT, ZT, XB, YB, ZB)
+    print(L1, L2, L3)
+    plotGraph(XT, YT, ZT, XB, YB, L1, L2, L3)
 
 
 def geval(l, b, d):
@@ -129,16 +124,67 @@ def YPequations(P, b, d):
     YP[2] = - 1 / 2.0 * (b + d - P[2])
     return YP
 
+def calcBaseX(b,d):
+    BX = np.zeros(6)
+    BX[0] = np.sqrt(3) * (2 * b + d)/6.0
+    BX[1] = -np.sqrt(3) * (b - d)/6.0
+    BX[2] = -np.sqrt(3) * (b + 2 * d)/6.0
+    BX[3] = -np.sqrt(3) * (b + 2 * d)/6.0
+    BX[4] = -np.sqrt(3) * (b - d)/6.0
+    BX[5] = np.sqrt(3) * (2 * b + d)/6.0
+    return BX
 
-def stf(XT, *data):
+def calcBaseY(b, d):
+    BY= np.zeros(6)
+    BY[0] = d / 2.0
+    BY[1] = (b + d) / 2.0
+    BY[2] = b / 2.0
+    BY[3] = -b / 2.0
+    BY[4] = -(b+d) / 2.0
+    BY[5] = -d / 2.0
+    return BY
+
+def calcLegs(xt,yt,zt,bx,by,bz):
+    lx = np.zeros((6,2))
+    ly = np.zeros((6,2))
+    lz = np.zeros((6,2))
+    for i in range(2):
+        lx[i] = np.array([bx[i],xt[0]])
+        ly[i] = np.array([by[i],yt[0]])
+        lz[i] = np.array([bz[i],zt[0]])
+    for i in range(2,4):
+        lx[i] = np.array([bx[i],xt[1]])
+        ly[i] = np.array([by[i],yt[1]])
+        lz[i] = np.array([bz[i],zt[1]])
+    for i in range(4,6):
+        lx[i] = np.array([bx[i],xt[2]])
+        ly[i] = np.array([by[i],yt[2]])
+        lz[i] = np.array([bz[i],zt[2]])
+    return lx,ly,lz
+
+def stf(XT, a, b, d, H, L, P, XP, YP):
     finalXT = np.zeros(3)
-    a, b, d, H, L, P, XP, YP = data
     finalXT[0] = a**2 + 2 * XT[0] * XT[1] - 2 * XT[0] * (XP[0] + np.sqrt(3) * (YP[0] - YP[1])) - 2 * XP[1] * XT[1] - ((np.sqrt(3) * XP[0] - YP[0] + YP[1])**2 + (H[0]**2 + H[1]**2) - 4 * XP[0]**2 - XP[1]**2) + 2 * np.sqrt((H[0]**2 - 4 * (XT[0] - XP[0])**2) * (H[1]**2 - (XT[1] - XP[1])**2))
     finalXT[1] = a**2 - 4 * XT[0] * XT[2] - 2 * XT[0] * (XP[0] - 3 * XP[2] + np.sqrt(3) * (YP[0] - YP[2])) - 2 * XT[2] * ((-3) * XP[0] + XP[2] + np.sqrt(3) * (YP[0] - YP[2])) - ((np.sqrt(3) * (XP[0] + XP[2]) - YP[0] + YP[2])**2 + (H[0]**2 + H[2]**2) - 4 * XP[0]**2 - 4 * XP[2]**2) + 2 * np.sqrt((H[0]**2 - 4 * (XT[0] - XP[0])**2) * (H[2]**2 - 4 * (XT[2] - XP[2])**2))
     finalXT[2] = a**2 + 2 * XT[1] * XT[2] - 2 * XT[2] * (XP[2] + np.sqrt(3) * (YP[1] - YP[2])) - 2 * XP[1] * XT[1] - ((np.sqrt(3) * XP[2] - YP[1] + YP[2])**2 + (H[1]**2 + H[2]**2) - XP[1]**2 - 4 * XP[2]**2) + 2 * np.sqrt((H[1]**2 - (XT[1] - XP[1])**2) * (H[2]**2 - 4 * (XT[2] - XP[2])**2))
+    
     return finalXT
 
+def plotGraph(XT, YT, ZT, XB, YB, LX, LY, LZ):
+    fig = plt.figure()
+  
+    ZB = np.zeros(6)
+    
+    ax = Axes3D(fig)
+    top = ax.plot_trisurf(XT, YT, ZT, color= 'blue')
+    bottom = ax.plot_trisurf(XB, YB, ZB, color= 'red')
+    for i in range(6):
+        ax.add_collection3d(ax.plot_wireframe(LX[i], LY[i], LZ[i], color = 'black'))
+    ax.add_collection3d(top)
+    ax.add_collection3d(bottom)
 
+    plt.show()
+    
 
 L = (8, 8, 8, 8, 8, 8)
 b = 15
@@ -149,3 +195,4 @@ L = (11.5, 11.5, 11.5, 11.5, 11.5, 11.5)
 a = 10
 task2(L, b, d, a)
 task4(L, b, d, a)
+task5(L, b, d, a)
