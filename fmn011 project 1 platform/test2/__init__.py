647720b187d138a271fdfa1c64a5be851e6d8d3f
Martin <martin.je.bergman@gmail.com>
2017-04-05 21:24:04 +0200

    jag vill pulla dig

--- fmn011 project 1 platform/test2/__init__.py
+++ fmn011 project 1 platform/test2/__init__.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import math
-from random import uniform
+
+
 
 
 
@@ -17,7 +18,7 @@ def task2(L, b, d, a):
     XT = (2.4537, -4.9075, 2.7424)
     print(XT)
     print stf(XT, L, b, d, a)
-    return
+    
 
 def task4(L, b, d, a):
     XT = (2.4537, -4.9075, 2.7424)
@@ -27,8 +28,13 @@ def task5(L, b, d, a):
     startXT = (2.4537, -4.9075, 2.7424)
     #Legs at min length:
     L = (8, 8, 8, 8, 8, 8)
-    calculatedXT = (fsolve(stf, startXT, (L,b,d,a)))
-    
+    XT = (fsolve(stf, startXT, (L,b,d,a)))
+    P = constructP(L, b, d)
+    H = constructH(L, P)
+    XP = XPequations(P, b, d)
+    YP = YPequations(P, b, d)
+    YT = calcYT(XP, XT, YP)
+    ZT = calcZT(H, XP, XT)
     #Legs at max length:
     L = (15, 15, 15, 15, 15, 15)
     calculatedXT = (fsolve(stf, startXT, (L,b,d,a)))
@@ -41,8 +47,7 @@ def task5(L, b, d, a):
     L = (8, 15, 8, 15, 8, 15)
     calculatedXT = (fsolve(stf, startXT, (L,b,d,a)))
     
-    
-        
+     
     
 def geval(l, b, d):
     L = l
@@ -54,8 +59,6 @@ def geval(l, b, d):
     print(XPequations(P, b, d))
     print("YP: ")
     print(YPequations(P, b, d))
-    
-    return
 
 def constructP(L, b, d):
     P = np.zeros(3)
@@ -69,7 +72,19 @@ def constructH(L,P):
         H[i] = math.sqrt(L[2 * (i+1) - 1]**2 - P[i]**2)
     return H
 
-
+def calcYT(XP, XT, YP):
+    YT = np.zeros(3)
+    YT[0] = np.sqrt(3) * XT[0] - (np.sqrt(3) * XP[0] - YP[0])
+    YT[1] = YP[1]
+    YT[2] = -np.sqrt(3) * XT[2] + (np.sqrt(3) * XP[2] + YP[2])
+    return YT
+    
+def calcZT(H, XP, XT):
+    ZT = np.zeros(3)
+    ZT[0] = np.sqrt(H[0]**2 - 4 * (XT[0] - XP[0])**2)
+    ZT[1] = np.sqrt(H[1]**2 - (XT[1] - XP[1])**2)
+    ZT[2] = np.sqrt(H[2]**2 - 4 * (XT[2] - XP[2])**2)
+    return ZT
 
 def XPequations(P, b, d):
     XP = np.zeros(3)
@@ -96,15 +111,12 @@ def stf(XT, L, b, d, a):
     finalXT[2] = a**2 + 2 * XT[1] * XT[2] - 2 * XT[2] * (XP[2] + np.sqrt(3) * (YP[1] - YP[2])) - 2 * XP[1] * XT[1] - ((np.sqrt(3) * XP[2] - YP[1] + YP[2])**2 + (H[1]**2 + H[2]**2) - XP[1]**2 - 4 * XP[2]**2) + 2 * np.sqrt((H[1]**2 - (XT[1] - XP[1])**2) * (H[2]**2 - 4 * (XT[2] - XP[2])**2))
     return finalXT
 
-def getXT(XT, L, b, d, a):
-    
-    return
-
 
 L = (8, 8, 8, 8, 8, 8)
 b = 15
 d = 1
-#task1(L, b, d)
+
+task1(L, b, d)
 L = (11.5, 11.5, 11.5, 11.5, 11.5, 11.5)
 a = 10
 task2(L, b, d, a)
